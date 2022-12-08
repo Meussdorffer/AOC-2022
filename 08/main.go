1bca530db1eddeb7e2f5680c9b7cd7377872d2f7
Jack Meussdorffer <jackcmv@gmail.com>
2022-12-07 22:37:12 -0800

    day 8 part 2 comments & cleanup

--- 08/main.go
+++ 08/main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+// parses a 2D array of ints from the problem input.
 func parseArray(input string) [][]int {
 	var arr [][]int
 	for _, line := range strings.Split(input, "\n") {
@@ -21,6 +22,7 @@ func parseArray(input string) [][]int {
 	return arr
 }
 
+// creates a transposed version of an array.
 func transposeArray(arr [][]int) [][]int {
 	nRows := len(arr)
 	nCols := len(arr[0])
@@ -37,6 +39,7 @@ func transposeArray(arr [][]int) [][]int {
 	return arrT
 }
 
+// convenience function for finding the max value in a slice.
 func max(slice []int) int {
 	val := slice[0]
 	for _, i := range slice {
@@ -47,6 +50,7 @@ func max(slice []int) int {
 	return val
 }
 
+// convenience function for finding the min value in a slice.
 func min(slice []int) int {
 	val := slice[0]
 	for _, i := range slice {
@@ -57,24 +61,18 @@ func min(slice []int) int {
 	return val
 }
 
-func main() {
-	inputFile := os.Args[1]
-	file, err := os.ReadFile(inputFile)
-	if err != nil {
-		fmt.Printf("Could not read the file due to error: %s \n", err)
+// convenience function for reversing a slice.
+func reverseSlice(slice []int) []int {
+	var reversedSlice []int
+	for i := len(slice) - 1; i >= 0; i-- {
+		reversedSlice = append(reversedSlice, slice[i])
 	}
 
-	arr := parseArray(string(file))
-	arrT := transposeArray(arr)
+	return reversedSlice
+}
 
-	// for _, line := range arr {
-	// 	fmt.Println(line)
-	// }
-	// println("\n")
-	// for _, line := range arrT {
-	// 	fmt.Println(line)
-	// }
-
+// determines the number of visible trees (part 1)
+func calcVisibleTrees(arr [][]int, arrT [][]int) int {
 	nRows := len(arr)
 	nCols := len(arr[0])
 	visibleTrees := (nRows * 2) + (nCols * 2) - 4
@@ -92,6 +90,59 @@ func main() {
 			}
 		}
 	}
-	println(visibleTrees)
 
+	return visibleTrees
+}
+
+// calculates the max scenic score for every tree in the array.
+func calcMaxScenicScore(arr [][]int, arrT [][]int) int {
+	nRows := len(arr)
+	nCols := len(arr[0])
+	maxScenicScore := 0
+	for rowIdx := 0; rowIdx < nRows; rowIdx++ {
+		for colIdx := 0; colIdx < nCols; colIdx++ {
+			scenicScore := 1
+			treeHeight := arr[rowIdx][colIdx]
+
+			// gather all trees in each direction; reverse slices where necessary for easier iteration.
+			leftSlice := reverseSlice(arr[rowIdx][0:colIdx])
+			rightSlice := arr[rowIdx][colIdx+1:]
+			upSlice := reverseSlice(arrT[colIdx][0:rowIdx])
+			downSlice := arrT[colIdx][rowIdx+1:]
+
+			// iterate over each element in the directional slice until a tree that blocks
+			// the current tree's view is encountered.
+			for _, slice := range [][]int{leftSlice, rightSlice, upSlice, downSlice} {
+				viewDistance := 0
+				for _, height := range slice {
+					viewDistance++
+					if height >= treeHeight {
+						break
+					}
+				}
+				scenicScore *= viewDistance
+			}
+
+			// save only the max scenicScore found.
+			if scenicScore > maxScenicScore {
+				maxScenicScore = scenicScore
+			}
+		}
+	}
+
+	return maxScenicScore
+}
+
+func main() {
+	inputFile := os.Args[1]
+	file, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Could not read the file due to error: %s \n", err)
+	}
+
+	arr := parseArray(string(file))
+	arrT := transposeArray(arr)
+
+	println(calcVisibleTrees(arr, arrT))
+	println(calcMaxScenicScore(arr, arrT))
 }
